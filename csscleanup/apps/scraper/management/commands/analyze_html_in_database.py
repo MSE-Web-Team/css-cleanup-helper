969ab85987182bdda49cf262670600bfa4c4c02d
derekvawdrey <derek.vawdrey@gmail.com>
2023-06-09 13:45:50 -0600

    Adding things

---
+++ csscleanup/apps/scraper/management/commands/analyze_html_in_database.py
@@ -0,0 +1,64 @@
+import datetime
+from django.core.management.base import BaseCommand, CommandError
+from csscleanup.apps.scraper.models import HtmlBaseUrl, HtmlElement, HtmlPage, HtmlLink, HtmlSource
+import os
+from bs4 import BeautifulSoup
+
+class Command(BaseCommand):
+    help = 'Analyzes all pages of parent base-url and inserts necessary information into the database for analysis'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--base-url', type=str)
+        parser.add_argument('--css-directory', type=str)
+        parser.add_argument('--js-directory', type=str)
+
+    def handle(self, *args, **options):
+        # code for html analysis
+        if options['base_url'] is None or options['css_directory'] is None or options['js_directory'] is None:
+            raise CommandError('Please provide base-url, css-directory, and js-directory flag')
+
+        css_directory = options['css_directory']
+        js_directory = options['js_directory']
+        # /Volumes/education22/Education/web/themes
+        # Gets the css and js file paths to analyze
+        css_file_paths = find_files(css_directory, ".css")
+        js_file_paths = find_files(js_directory, ".js")
+
+        # Get all pages stored in the database by base url
+        base_url = HtmlBaseUrl.objects.get(url = options['base_url'])
+        html_pages = HtmlPage.objects.filter(related_base_url = base_url)
+        for page in html_pages:
+            self.analyzeSources(base_url, page, css_file_paths, js_file_paths)
+
+    def analyzeSources(self, related_base_url, page, css_file_paths, js_file_paths):
+        # Analyze all <link> and <script> tags for sources and inserts used ones into database
+        soup = BeautifulSoup(page.html, 'html.parser')
+
+        # Get all script and link tags with their sources inside the page html
+        script_tags = []
+        link_tags = []
+        for script_tag in soup.find_all('script'):
+            src = script_tag.get('src')
+            if src:
+                script_tags.append(src)
+                obj, created = HtmlSource.objects.get_or_create(name='foo', defaults={'source_name': src})
+
+        for link_tag in soup.find_all('link'):
+            href = link_tag.get('href')
+            if href:
+                link_tags.append(href)
+
+        
+        
+
+
+def find_files(directory, file_extension):
+    files_found = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_map = {"file_path":"","file_name":""}
+            if file.endswith(file_extension):
+                file_map["file_name"] = os.path.basename(os.path.join(root, file))
+                file_map["file_path"] = os.path.join(root, file)
+                files_found.append(file_map)
+    return files_found
--- csscleanup/apps/scraper/models/html_models.py
+++ csscleanup/apps/scraper/models/html_models.py
@@ -4,11 +4,17 @@ class HtmlBaseUrl(models.Model):
     url = models.CharField(max_length=200, default="")
     is_scraped = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.url
+
 class HtmlPage(models.Model):
     related_base_url = models.ForeignKey(HtmlBaseUrl, on_delete=models.CASCADE)
     page_url = models.CharField(max_length=200, default="")
     title = models.CharField(max_length=300, default="")
     html = models.TextField(default = "")
+
+    def __str__(self):
+        return self.title
 
 # Stores the base url
 class HtmlElement(models.Model):
@@ -19,4 +25,9 @@ class HtmlElement(models.Model):
 class HtmlLink(models.Model):
     related_html_page = models.ForeignKey(HtmlPage, on_delete=models.CASCADE)
     link = models.CharField(max_length=200, default="")
+
+class HtmlSource(models.Model):
+    related_base_url = models.ForeignKey(HtmlBaseUrl, on_delete=models.CASCADE)
+    source = models.CharField(max_length=500, default="")
+    used = models.BooleanField(default=False)
 
